4d8472909c02343b660cb81cd1a4e464e6d47b16
sateeshlevelup <112386309+sateeshlevelup@users.noreply.github.com>
2023-03-26 21:52:18 +0530

    Second Demo in Class 1
    This file contains Code to Create VM on AWS and Azure at the Sametime

---
+++ main.tf
@@ -0,0 +1,191 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.43.0"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+
+
+provider "azurerm" {
+    subscription_id = "b9c1d0b3-cd3c-401e-8931-668b667e4ace"
+    tenant_id = "806d981e-b582-4a31-83ff-31c9bdc87fa6"
+    client_id = "dafdb158-2738-4d95-aa71-bbda9db9f04d"
+    client_secret = "jOh8Q~mXnNIrrByNxF~gBZ_TrU1.Nat.JcGhhaj."
+    features {
+      
+    }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "single-instance"
+
+  ami                    = "ami-0dfcb1ef8550277af"
+  instance_type          = "t2.micro"
+  
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "West Europe"
+}
+
+locals {
+  resource_group_name="app-grp"
+  location="North Europe"
+  virtual_network={
+    name="app-network"
+    address_space="10.0.0.0/16"
+  }
+
+  subnets=[
+    {
+      name="subnetA"
+      address_prefix="10.0.0.0/24"
+    },
+    {
+      name="subnetB"
+      address_prefix="10.0.1.0/24"
+    }
+  ]
+}
+
+resource "azurerm_resource_group" "appgrp" {
+  name     = local.resource_group_name
+  location = local.location  
+}
+
+resource "azurerm_virtual_network" "appnetwork" {
+  name                = local.virtual_network.name
+  location            = local.location
+  resource_group_name = local.resource_group_name
+  address_space       = [local.virtual_network.address_space]  
+ 
+   depends_on = [
+     azurerm_resource_group.appgrp
+   ]
+  }
+
+
+  resource "azurerm_subnet" "subnetA" {
+  name                 = local.subnets[0].name
+  resource_group_name  = local.resource_group_name
+  virtual_network_name = local.virtual_network.name
+  address_prefixes     = [local.subnets[0].address_prefix]
+  depends_on = [
+    azurerm_virtual_network.appnetwork
+  ]
+}
+
+resource "azurerm_subnet" "subnetB" {
+  name                 = local.subnets[1].name
+  resource_group_name  = local.resource_group_name
+  virtual_network_name = local.virtual_network.name
+  address_prefixes     = [local.subnets[1].address_prefix]
+  depends_on = [
+    azurerm_virtual_network.appnetwork
+  ]
+}
+
+resource "azurerm_network_interface" "appinterface" {
+  name                = "appinterface"
+  location            = local.location
+  resource_group_name = local.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnetA.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.appip.id
+  }
+  depends_on = [
+    azurerm_subnet.subnetA
+  ]
+}
+
+resource "azurerm_public_ip" "appip" {
+  name                = "app-ip"
+  resource_group_name = local.resource_group_name
+  location            = local.location
+  allocation_method   = "Static"
+ depends_on = [
+   azurerm_resource_group.appgrp
+ ]
+}
+
+resource "azurerm_network_security_group" "appnsg" {
+  name                = "app-nsg"
+  location            = local.location
+  resource_group_name = local.resource_group_name
+
+  security_rule {
+    name                       = "AllowRDP"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  depends_on = [
+    azurerm_resource_group.appgrp
+  ]
+}
+
+resource "azurerm_subnet_network_security_group_association" "appnsglink" {
+  subnet_id                 = azurerm_subnet.subnetA.id
+  network_security_group_id = azurerm_network_security_group.appnsg.id
+}
+
+resource "azurerm_windows_virtual_machine" "appvm" {
+  name                = "appvm"
+  resource_group_name = local.resource_group_name
+  location            = local.location
+  size                = "Standard_D2S_v3"
+  admin_username      = "adminuser"
+  admin_password      = "Azure@123"
+  network_interface_ids = [
+    azurerm_network_interface.appinterface.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+  depends_on = [
+    azurerm_network_interface.appinterface,
+    azurerm_resource_group.appgrp
+  ]
+}
+
+
+
