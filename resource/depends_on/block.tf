b253ab842d8b83fc3134ba2ca9693c727a86f6af
sateeshlevelup <112386309+sateeshlevelup@users.noreply.github.com>
2023-10-24 14:56:42 +0530

    Create block.tf

---
+++ resource/depends_on/block.tf
@@ -0,0 +1,17 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.4"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
